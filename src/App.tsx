1826391b4f7e6ac258a667777efa0600cb1d6be0
shivam-09-sinha <97621729+shivam-09-sinha@users.noreply.github.com>
2022-03-14 07:21:44 +0530

    delete it after use

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Todos from './components/Todos';
+
+
+function App() {
+  return (
+    <div>
+     <Todos items={['Learn React','Learn Typescript','Learn about DOM']}/>
+    </div>
+  );
+}
+
+export default App;
